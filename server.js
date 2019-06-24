f0e08330adb339a6fddd585ad8c297deb6dcebe2
Ethan <ebonsignori@gmail.com>
2019-06-24 16:08:59 -0400

    File structure

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+// Scraping tools
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+// Loads all models
+const db = require('./models')
+
+const PORT = 3000
+
+const app = express()
+
+// Middleware
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static('public'))
+
+// Routes
+require('./routes/routes')(app)
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines'
+
+mongoose.connect(MONGODB_URI)
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`)
+})
